66fe4fe8e628f00efc97ec0c6e266e5d1e00da1f
David Skrundz <david@skrundz.ca>
2017-02-01 12:51:45 -0700

    Change dependency information

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ import PackageDescription
 let package = Package(
 	name: "LibC",
 	dependencies: [
-		.Package(url: "https://github.com/DavidSkrundz/CLibC.git", versions: Version(1,0,0)..<Version(1,1,0)),
-		.Package(url: "https://github.com/DavidSkrundz/UnicodeOperators.git", versions: Version(1,0,0)..<Version(1,1,0)),
+		.Package(url: "https://github.com/DavidSkrundz/CLibC.git", majorVersion: 1, minor: 0),
+		.Package(url: "https://github.com/DavidSkrundz/UnicodeOperators.git", majorVersion: 1, minor: 0),
 	]
 )
 
